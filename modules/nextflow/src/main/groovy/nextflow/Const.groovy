6f6ba6b41dea92ff6b88627747b1d63ce1a0bd96
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-06-27 06:03:08 +0200

    Bump 19.04.3-SNAPSHOT

--- modules/nextflow/src/main/groovy/nextflow/Const.groovy
+++ modules/nextflow/src/main/groovy/nextflow/Const.groovy
@@ -52,17 +52,17 @@ class Const {
     /**
      * The application version
      */
-    static public final String APP_VER = "19.04.2"
+    static public final String APP_VER = "19.04.3-SNAPSHOT"
 
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1561132930076
+    static public final long APP_TIMESTAMP = 1561607460949
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5078
+    static public final int APP_BUILDNUM = 5081
 
 
     /**
